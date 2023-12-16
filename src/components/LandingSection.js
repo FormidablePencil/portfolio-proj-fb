dc9d4262f61a6c46b5b9f9c752f715cba313a987
Dennis Aleksandrov <formidablepencil@gmail.com>
2023-12-16 09:58:18 -0800

    avatar & into to bio

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Dennis!";
+const greeting = "Hello, I am Pete!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
@@ -15,6 +15,19 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+    <VStack spacing={3}>
+      <Avatar size="lg" src="https://i.pravatar.cc/150?img=7" />
+      <Heading as="h2" size="sm" textAlign="center" color="white">
+        {greeting}
+      </Heading>
+      <Heading as="h1" size="lg" textAlign="center" color="white">
+        {bio1}
+      </Heading>
+      <Heading as="h1" size="lg" textAlign="center" color="white">
+        {bio2}
+      </Heading>
+
+    </VStack>
 
   </FullScreenSection>
 );
